6d92d5752f508e694f6472c37494571b9d220997
George Cooper Jones <gcjones1@asu.edu>
2018-01-21 16:51:50 -0700

    First Commit
    Driving Simulation Unity Project

---
+++ EasySuspension/Assets/SteeringWheelUI/Scripts/GraphBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphBar : MonoBehaviour {
+	public string AxisName;
+	private Vector3 position;
+	// Use this for initialization
+	void Start () {
+		position = this.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newYScale = (Input.GetAxis (AxisName) + 1f)/5f * .2f;
+		this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x, newYScale, this.gameObject.transform.localScale.z);
+		this.gameObject.transform.localPosition = new Vector3(position.x, position.y + newYScale/2, position.z);
+	}
+}
---
+++ EasySuspension/Assets/SteeringWheelUI/Scripts/Selector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selector : MonoBehaviour {
+	public GameObject selector;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float gear = Input.GetAxis ("GearSwitch");
+		if (gear == 0) {
+			selector.transform.localPosition = new Vector3 (.337f, 0f, 0f);
+		} else if (gear > 0) {
+			selector.transform.localPosition = new Vector3 (.337f, -0.1f, 0f);
+		} else {
+			selector.transform.localPosition = new Vector3 (.337f, 0.1f, 0f);
+		}
+	}
+}
