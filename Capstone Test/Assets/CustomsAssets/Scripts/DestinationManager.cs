80e66c27ad1e920c29e972b417e8ef55e423ab84
George Cooper Jones <gcjones1@asu.edu>
2018-03-18 21:58:21 -0700

    NowTheresPlacesToGo
    Added Destination Manager
    Added Waypoints

---
+++ Capstone Test/Assets/CustomsAssets/Scripts/DestinationManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationManager : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    public int currentWaypoint = 0;
+
+    private GameObject player;
+    private CarControlCS carControl;
+
+    
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        carControl = player.GetComponent<CarControlCS>();
+        carControl.Destination = waypoints[currentWaypoint].transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckWaypoint();
+	}
+
+    private void CheckWaypoint()
+    {
+        if (currentWaypoint < waypoints.Length)
+        {
+            if (Vector3.Distance(player.transform.position, waypoints[currentWaypoint].transform.position) < 5)
+            {
+                //Move to next waypoint
+                currentWaypoint++;
+                carControl.Destination = waypoints[currentWaypoint].transform.position;
+            }
+        }
+    }
+}
