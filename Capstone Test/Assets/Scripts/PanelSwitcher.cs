2fde6b4f9e1aa319c00d08f559aaab308d71359f
ohiader <ryan.black@ryblack.com>
2018-04-03 14:37:41 -0700

    added night map, fixed doverpointe, new menu scene is MenuPanels or whatever

---
+++ Capstone Test/Assets/Scripts/PanelSwitcher.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PanelSwitcher : MonoBehaviour {
+
+	public GameObject mainPanel, deliveryPanel, racingPanel, instructionsPanel;
+
+	public void ToMainMenu () {
+		if (mainPanel.activeSelf == false) {
+			mainPanel.SetActive (true);
+		}
+		if (deliveryPanel.activeSelf == true) {
+			deliveryPanel.SetActive (false);
+		}
+		if (racingPanel.activeSelf == true) {
+			racingPanel.SetActive (false);
+		}
+		if (instructionsPanel.activeSelf == true) {
+			instructionsPanel.SetActive (false);
+		}
+	}
+
+	public void ToDeliveryLevels () {
+		if (deliveryPanel.activeSelf == false) {
+			deliveryPanel.SetActive (true);
+		}
+		if (mainPanel.activeSelf == true) {
+			mainPanel.SetActive (false);
+		}
+		if (racingPanel.activeSelf == true) {
+			racingPanel.SetActive (false);
+		}
+		if (instructionsPanel.activeSelf == true) {
+			instructionsPanel.SetActive (false);
+		}
+	}
+
+	public void LoadLvlOne () {
+		SceneManager.LoadScene ("doverpointe");
+	}
+
+	public void LoadLvlTwo () {
+		SceneManager.LoadScene ("NightMap");
+	}
+
+	public void LoadTrackOne () {
+		SceneManager.LoadScene ("RacingLoop");
+	}
+
+	public void LoadTrackTwo () {
+		SceneManager.LoadScene ("Racing2");
+	}
+
+	public void ToInstructions () {
+		if (instructionsPanel.activeSelf == false) {
+			instructionsPanel.SetActive (true);
+		}
+		if (deliveryPanel.activeSelf == true) {
+			deliveryPanel.SetActive (false);
+		}
+		if (racingPanel.activeSelf == true) {
+			racingPanel.SetActive (false);
+		}
+		if (mainPanel.activeSelf == true) {
+			mainPanel.SetActive (false);
+		}
+	}
+
+	public void ToRacingLevels () {
+		if (racingPanel.activeSelf == false) {
+			racingPanel.SetActive (true);
+		}
+		if (deliveryPanel.activeSelf == true) {
+			deliveryPanel.SetActive (false);
+		}
+		if (mainPanel.activeSelf == true) {
+			mainPanel.SetActive (false);
+		}
+		if (instructionsPanel.activeSelf == true) {
+			instructionsPanel.SetActive (false);
+		}
+	}
+}
