0862dd30dd793fbf6f34c7cf5be067312a883514
Excoh <excel.ortega@gmail.com>
2018-04-10 03:05:13 -0700

    more networking fix

---
+++ Capstone Test/Assets/Scripts/Networking/NetworkObjectDisabler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// Sets the game object this script is attached to if it is not the local player
+public class NetworkObjectDisabler : MonoBehaviour 
+{
+    public NetworkIdentity identity;
+	// Use this for initialization
+	void Start () {
+        
+        if (!identity.isLocalPlayer)
+        {
+            this.gameObject.SetActive(false);
+            return;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
